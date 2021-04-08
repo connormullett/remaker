7ae529ad0416d8926029a6c623a6cac7e7b2c2ed
connor <connormullett@gmail.com>
2021-04-07 23:39:12 -0400

    find_remake_file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{env::current_exe, path::PathBuf};
+
+use std::env;
+use std::io;
+use std::process;
+
+const REMAKE_FILE_NAME: &'static str = "remake";
+
+fn find_remake_file() -> io::Result<PathBuf> {
+    let mut current_dir = env::current_dir()?;
+    current_dir.push(REMAKE_FILE_NAME);
+
+    if let false = current_dir.exists() {
+        return Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "remake file not found",
+        ));
+    }
+
+    Ok(current_dir)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let remake_file = match find_remake_file() {
+        Ok(file) => file,
+        Err(error) => {
+            println!("{}", error);
+            process::exit(1);
+        }
+    };
 }
