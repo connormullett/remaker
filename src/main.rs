4159ce4655d0fc74eb850e97aa81182cb4c670ee
connor <connormullett@gmail.com>
2021-04-11 01:33:14 -0400

    parse and construct wildcards

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, path::PathBuf};
+use std::{error::Error, ops::Rem, path::PathBuf};
 
 use std::{env, fs, io, process};
 
@@ -11,6 +11,54 @@ use pest::Parser;
 #[derive(Parser)]
 #[grammar = "remake.pest"]
 pub struct RemakeParser;
+
+#[derive(Debug, Clone)]
+struct RemakeRule<'a> {
+    targets: Vec<&'a str>,
+    dependencies: Vec<&'a str>,
+    build_commands: Vec<&'a str>,
+}
+
+impl<'a> RemakeRule<'a> {
+    pub fn new() -> Self {
+        Self {
+            targets: vec![],
+            dependencies: vec![],
+            build_commands: vec![],
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.targets = vec![];
+        self.dependencies = vec![];
+        self.build_commands = vec![];
+    }
+}
+
+#[derive(Debug, Clone)]
+struct RemakeWildcard<'a> {
+    symbol: &'a str,
+    values: Vec<&'a str>,
+}
+
+impl<'a> RemakeWildcard<'a> {
+    pub fn new() -> Self {
+        Self {
+            symbol: "",
+            values: vec![],
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.symbol = "";
+        self.values = vec![];
+    }
+}
+
+struct RemakeFile<'a> {
+    rules: Vec<RemakeRule<'a>>,
+    wildcards: Vec<RemakeWildcard<'a>>,
+}
 
 const REMAKE_FILE_NAME: &str = "remaker";
 
@@ -51,7 +99,45 @@ fn main() {
         .next()
         .unwrap();
 
+    let mut wildcards = Vec::<RemakeWildcard>::new();
+
+    let mut rules = Vec::<RemakeRule>::new();
+    let mut current_rule = RemakeRule::new();
+
     for line in file.into_inner() {
-        println!("{:#?}", line);
+        match line.as_rule() {
+            Rule::wildcard => {
+                let mut inner_rules = line.into_inner();
+                let symbol: &str = inner_rules.next().unwrap().as_str();
+                let mut current_wildcard = RemakeWildcard::new();
+                current_wildcard.symbol = symbol;
+
+                while let Some(value) = inner_rules.next() {
+                    current_wildcard.values.push(value.as_str());
+                }
+                wildcards.push(current_wildcard.clone());
+                current_wildcard.clear();
+            }
+            Rule::target_line => {
+                rules.push(current_rule.clone());
+                current_rule.clear();
+                let mut inner_rules = line.into_inner();
+                let target = inner_rules.next().unwrap().as_str();
+                let dependencies = inner_rules.next().unwrap().as_str();
+                current_rule = RemakeRule {
+                    targets: vec![target],
+                    dependencies: vec![dependencies],
+                    build_commands: vec![],
+                };
+            }
+            Rule::build_command => {
+                let rules = line.into_inner();
+                current_rule.build_commands.push(rules.as_str())
+            }
+            Rule::EOI => (),
+            _ => (),
+        }
     }
+
+    println!("wildcards {:#?}", wildcards);
 }
