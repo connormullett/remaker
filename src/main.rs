423b58851edcf3c94ccda02fcd821c511d865aa0
connor <connormullett@gmail.com>
2021-04-11 23:09:48 -0400

    handy note

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,7 @@ fn main() {
 
     let mut remake_file = parse::parse(&remake_file_contents);
 
+    // TODO: expand placeholders in targets and dependencies
     remake_file.handle_wildcards();
 
     let default_rule = match matches.value_of("RULE") {
